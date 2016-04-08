16f1b4beee6326d47de13dfaaa7b9124784d5f51
codewalker <codewalker@hardkernel.com>
2016-04-08 10:54:50 +0900

    ODROID-C2: Rollback to adjust icon position.
    Change-Id: I50bfb1e7667661812bbda1aad76a41b234c2b508

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1249,28 +1249,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mNavigationBarView.getVolUpButton().setOnClickListener(mVolUpClickListener);
             mNavigationBarView.getShutdownButton().setOnClickListener(mShutdownClickListener);
             mNavigationBarView.getScreenshotButton().setOnClickListener(mScreenshotClickListener);
-            int rotation = SystemProperties.getInt("ro.sf.hwrotation", 0);
-            if (rotation == 90 || rotation == 270) {
-                View view = mNavigationBarView.getVolUpButton();
-                view.setLayoutParams(new LinearLayout.LayoutParams(
-                            40, LayoutParams.MATCH_PARENT));
-                view.setPadding(0, view.getPaddingTop(), 0, view.getPaddingBottom());
-
-                view = mNavigationBarView.getVolDownButton();
-                view.setLayoutParams(new LinearLayout.LayoutParams(
-                            40, LayoutParams.MATCH_PARENT));
-                view.setPadding(0, view.getPaddingTop(), 0, view.getPaddingBottom());
-
-                view = mNavigationBarView.getShutdownButton();
-                view.setLayoutParams(new LinearLayout.LayoutParams(
-                            40, LayoutParams.MATCH_PARENT));
-                view.setPadding(0, view.getPaddingTop(), 0, view.getPaddingBottom());
-
-                view = mNavigationBarView.getScreenshotButton();
-                view.setLayoutParams(new LinearLayout.LayoutParams(
-                            40, LayoutParams.MATCH_PARENT));
-                view.setPadding(0, view.getPaddingTop(), 0, view.getPaddingBottom());
-            }
         }
     }
 
