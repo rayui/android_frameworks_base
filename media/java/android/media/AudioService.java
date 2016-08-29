a91518f7d801f54a503c95f46e2053a389fa4c3f
codewalker <codewalker@hardkernel.com>
2016-08-29 18:24:03 +0900

    Revert "ODROID-XU3/4: Remove save media volume limit warning."
    This reverts commit 85158bb5e26f3154a7573402b06303c1f2fb684d.
    
    Change-Id: I46220bd8f799679b4c28cbe4c5ef6beb4d1e8d73

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -510,7 +510,7 @@ public class AudioService extends IAudioService.Stub {
 
         mSafeMediaVolumeState = new Integer(Settings.Global.getInt(mContentResolver,
                                                         Settings.Global.AUDIO_SAFE_VOLUME_STATE,
-                                                        SAFE_MEDIA_VOLUME_INACTIVE));
+                                                        SAFE_MEDIA_VOLUME_NOT_CONFIGURED));
         // The default safe volume index read here will be replaced by the actual value when
         // the mcc is read by onConfigureSafeVolume()
         mSafeMediaVolumeIndex = mContext.getResources().getInteger(
