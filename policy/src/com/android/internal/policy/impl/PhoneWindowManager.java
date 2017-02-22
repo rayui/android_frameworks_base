3e592fad7a96331285246609b08a2579d1bbad9b
Luke Go <luke.go@hardkernel.com>
2017-02-22 11:23:34 +0900

    ODROID-C2: Added home button feature to shortcut.
    Change-Id: I2f9a1f9787f195dc657f16d8dfe7ed1e5a44be8a

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -2785,10 +2785,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
         }
 
-        // Setted Shortcut keys.
+        // Set Shortcut keys.
         if (down && !keyguardOn) {
             Intent intent = sApplicationShortcutTable.get(keyCode);
             if (intent != null) {
+                if (intent.getPackage().equals("home")) {
+                    launchHomeFromHotKey();
+                }
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 try {
                     startActivityAsUser(intent, UserHandle.CURRENT);
