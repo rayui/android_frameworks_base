0d2e4ba7750d99cc5a38c55df646ee9ccfde4b22
Luke Go <luke.go@hardkernel.com>
2017-07-04 18:11:59 +0900

    ODROID-N1: Added WRITE_MEDIA_STORAGE permission to all packages.
    - Accessing to the External Media, like SD card & HDD, is restricted on
      Android 6.0.1. So, We permitted it to use SD card or external media.
    
    Conflicts:
    	services/core/java/com/android/server/pm/Settings.java
    
    Change-Id: I1d5b1aad37cf96caf1094857315b1f733aff6670

--- services/core/java/com/android/server/pm/Settings.java
+++ services/core/java/com/android/server/pm/Settings.java
@@ -17,6 +17,7 @@
 package com.android.server.pm;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_MEDIA_STORAGE;
 import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DEFAULT;
 import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
 import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
@@ -2121,6 +2122,8 @@ final class Settings {
             return;
         }
 
+        boolean isMediaStoragePermission = false;
+
         serializer.startTag(null, TAG_PERMISSIONS);
 
         for (PermissionState permissionState : permissionStates) {
@@ -2128,6 +2131,19 @@ final class Settings {
             serializer.attribute(null, ATTR_NAME, permissionState.getName());
             serializer.attribute(null, ATTR_GRANTED, String.valueOf(permissionState.isGranted()));
             serializer.attribute(null, ATTR_FLAGS, Integer.toHexString(permissionState.getFlags()));
+            serializer.endTag(null, TAG_ITEM);
+
+            if (!isMediaStoragePermission
+                    && permissionState.getName().equals(WRITE_MEDIA_STORAGE)) {
+                isMediaStoragePermission = true;
+            }
+        }
+
+        if (!isMediaStoragePermission) {
+            serializer.startTag(null, TAG_ITEM);
+            serializer.attribute(null, ATTR_NAME, WRITE_MEDIA_STORAGE);
+            serializer.attribute(null, ATTR_GRANTED, "true");
+            serializer.attribute(null, ATTR_FLAGS, Integer.toHexString(0));
             serializer.endTag(null, TAG_ITEM);
         }
 
