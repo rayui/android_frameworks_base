5507ef4d53189dce518a420d09c7bcf3a56464c8
codewalker <codewalker@hardkernel.com>
2017-09-19 15:31:10 +0900

    Revert "SystemUI:when box ,select TvStatusBar"
    This reverts commit d33295eb117d8a9567b0e1349a8a48ae39c633c9.
    
    Change-Id: I817241f1a7372c9d5f038cf01461571dec9c8bc9

--- packages/SystemUI/src/com/android/systemui/statusbar/SystemBars.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SystemBars.java
@@ -25,7 +25,7 @@ import com.android.systemui.SystemUI;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
-import android.os.SystemProperties;
+
 /**
  * Ensure a single status bar service implementation is running at all times.
  *
@@ -86,10 +86,7 @@ public class SystemBars extends SystemUI implements ServiceMonitor.Callbacks {
 
     private void createStatusBarFromConfig() {
         if (DEBUG) Log.d(TAG, "createStatusBarFromConfig");
-        String clsName = mContext.getString(R.string.config_statusBarComponent);
-        if ("box".equals(SystemProperties.get("ro.target.product", "tablet"))){
-            clsName = "com.android.systemui.statusbar.tv.TvStatusBar";
-        }
+        final String clsName = mContext.getString(R.string.config_statusBarComponent);
         if (clsName == null || clsName.length() == 0) {
             throw andLog("No status bar component configured", null);
         }
